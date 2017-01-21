c2d270b7c1b67b1bc54837b3b7570c431bc5f6bc
Thomas Paviot <tpaviot@gmail.com>
2017-01-21 11:35:05 +0100

    fixed build for oce-0.17.2

--- oce-0.17.2/build.sh
+++ oce-0.17.2/build.sh
@@ -8,30 +8,36 @@ mkdir build
 cd build
 
 # Configure step
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
+cmake -G Ninja -DCMAKE_INSTALL_PREFIX=$PREFIX \
  -DCMAKE_BUILD_TYPE=Release \
  -DOCE_TESTING=OFF \
+ -DOCE_USE_PCH=ON \
+ -DOCE_COPY_HEADERS_BUILD=ON \
  -DCMAKE_PREFIX_PATH=$PREFIX \
  -DCMAKE_SYSTEM_PREFIX_PATH=$PREFIX \
  -DOCE_INSTALL_LIB_DIR=lib \
  -DOCE_INSTALL_BIN_DIR=bin \
  -DOCE_WITH_FREEIMAGE=ON \
+ -DFREEIMAGE_INCLUDE_DIR=$PREFIX/include/ \
  -DOCE_WITH_GL2PS=ON \
+ -DGL2PS_INCLUDE_DIR=$PREFIX/include/ \
  -DOCE_WITH_VTK=OFF \
  -DOCE_MULTITHREAD_LIBRARY=TBB \
  -DOCE_INSTALL_PREFIX=$PREFIX -DOCE_ENABLE_DEB_FLAG=OFF ..
 
 # Build step
-if [ `uname` == Darwin ]; then
-  make -j 5 | grep Built  # set to 5 on travis
-else
-  make -j $CPU_COUNT | grep Built
-fi
+ninja | grep Linking
+#if [ `uname` == Darwin ]; then
+  #make -j 5 | grep Built  # set to 5 on travis
+#else
+  #make -j $CPU_COUNT | grep Built
+#fi
 # Install step
-make install > installed_files.log  # to reduce the number of lines to the console
+ninja install > installed_files.txt
+#make install > installed_files.log  # to reduce the number of lines to the console
 
 if [ `uname` != Darwin ]; then
-    python $RECIPE_DIR/remove-system-libs.py $PREFIX/lib/oce-0.17/OCE-libraries-release.cmake
+    python $RECIPE_DIR/remove-system-libs.py $PREFIX/lib/oce-0.17.2/OCE-libraries-release.cmake
 else
-    python $RECIPE_DIR/remove-system-libs.py $PREFIX/OCE.framework/Versions/0.17/Resources/OCE-libraries-release.cmake
+    python $RECIPE_DIR/remove-system-libs.py $PREFIX/OCE.framework/Versions/0.17.2/Resources/OCE-libraries-release.cmake
 fi
